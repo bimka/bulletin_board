5006544b2211265857609472c0f0131809fb8135
Dmitriy Begishev <mintdragonevo@gmail.com>
2023-02-14 11:29:30 +0300

    Added connection to DB

---
+++ Helpers/ApplicationContext - Copy.cs
@@ -0,0 +1,27 @@
+﻿using BulletinBoard.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoard.Helpers
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=bulletinBoardDB;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Name = "Dima", Email = "dima@gmail.com", BirthDay = new DateTime(1992, 04, 27) },
+                new User { Id = 2, Name = "Masha", Email = "masha@gmail.com", BirthDay = new DateTime(1967, 06, 27) },
+                new User { Id = 3, Name = "Natasha", Email = "natasha@gmail.com", BirthDay = new DateTime(1989, 06, 07) }
+            );
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,64 +1,61 @@
-using BulletinBoard.Models;
 using System;
 using System.Xml.Linq;
+using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+using BulletinBoard.Models;
+using BulletinBoard.Helpers;
+
+var builder = WebApplication.CreateBuilder();
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connection));
+
 var app = builder.Build();
 
-List<User> users = new List<User>
-    {
-    new() { Id = 1, Name = "Dima", Email = "dima@gmail.com", BirthDay = new DateTime(1992,04,27) },
-    new() { Id = 2, Name = "Masha", Email = "masha@gmail.com", BirthDay = new DateTime(1967,06,27) },
-    new() { Id = 3, Name = "Natasha", Email = "natasha@gmail.com", BirthDay = new DateTime(1989,06,07) }
-    };
+app.MapGet("/", (ApplicationContext db) => db.Users.ToList());
 
-int id_counter = 4;
-
-app.MapGet("/", () => users);
-
-app.MapGet("/user/{id}", (int id) =>
+app.MapGet("/user/{id}", (int id, ApplicationContext db) =>
 {
-    User? user = users.FirstOrDefault(user => user.Id == id);
+    User? user = db.Users.FirstOrDefault(user => user.Id == id);
 
     if (user == null) return Results.NotFound(new { message = "Пользователь с таким id не найден" });
+
 
     return Results.Ok(user);
 });
 
-app.MapPost("/user/create", (User newUser) =>
+app.MapPost("/user/create", (User newUser, ApplicationContext db) =>
 {
-    newUser.Id = id_counter;
-    users.Add(newUser);
-
-    id_counter++;
+    db.Users.Add(newUser);
+    db.SaveChanges(); 
 
     return Results.Ok(newUser);
 });
 
-app.MapPut("/user/{id}/update", (int id, User userData) =>
+app.MapPut("/user/{id}/update", (int id, User userData, ApplicationContext db) =>
 {
-    User? userToUpdate = users.FirstOrDefault(user => user.Id == id);
+    User? userToUpdate = db.Users.FirstOrDefault(user => user.Id == id);
 
     if (userToUpdate == null) return Results.NotFound(new { message = "Пользователь с таким id не найден" });
 
-    if (userData.Name != null)
-    {
-        userToUpdate.Name = userData.Name;
-    }
+    if (userData.Name != null) userToUpdate.Name = userData.Name;      
+    
     if (userData.Email != null) userToUpdate.Email = userData.Email;
 
+    db.SaveChanges();
     return Results.Ok(userToUpdate);
 });
 
-app.MapDelete("/user/{id}/delete", (int id) =>
+app.MapDelete("/user/{id}/delete", (int id, ApplicationContext db) =>
 {
-    User? userToDelete = users.FirstOrDefault(user => user.Id == id);
+    User? userToDelete = db.Users.FirstOrDefault(user => user.Id == id);
 
     if (userToDelete == null) return Results.NotFound(new { message = "Пользователь с таким id не найден" });
 
-    users.Remove(userToDelete);
+    db.Users.Remove(userToDelete);
+    db.SaveChanges();
 
     return Results.Ok(userToDelete);
 });
 
 app.Run("http://localhost:4000");
+
